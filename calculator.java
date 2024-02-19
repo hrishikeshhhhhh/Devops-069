389ab0b556a2aebd838a37e7beaff6bc5ec2794d
Hrishi <01fe22bca069@kletech.ac.in>
2024-02-19 15:36:29 +0530

    updated code for square and cube operations

--- calculator.java
+++ calculator.java
@@ -1,4 +1,4 @@
-public class calculator {
+public class calculator extends ext_calculator {
     
     // Addition method
     public static float add(float num1, float num2) {
@@ -41,5 +41,7 @@ public class calculator {
         System.out.println("Subtraction: " + subtractionResult);
         System.out.println("Multiplication: " + multiplicationResult);
         System.out.println("Division: " + divisionResult);
+        System.out.println("Square : "+ square(operand1));
+        System.out.println("Cube: "+ cube(operand1));
     }
 }
---
+++ ext_calculator.java
@@ -0,0 +1,27 @@
+/**
+ * The class `ext_calculator` contains methods to calculate the square and cube of a given float
+ * number.
+ */
+public class ext_calculator {
+    /**
+     * The function "square" takes a float input and returns the square of that input as a float.
+     * 
+     * @param x The parameter `x` in the `square` function represents the number that you want to
+     * square.
+     * @return The function `square` is returning the square of the input parameter `x`.
+     */
+    static float square (float x){
+        return (x*x);
+    }
+    /**
+     * The `cube` function in Java calculates the cube of a given float number.
+     * 
+     * @param x The parameter `x` in the `cube` function represents the number that you want to
+     * calculate the cube of.
+     * @return The function `cube` is returning the cube of the input parameter `x`. It calculates
+     * `x*x*x` and returns the result as a float.
+     */
+    static float cube (float x){
+        return (x*x*x);
+    }
+}
